16f38b7a4183e12da9d0cacc5781caee5c0885fe
IgorF1999 <goncalo.shakes@hotmail.com>
2020-01-16 15:26:22 +0000

    update api.js

--- projeto/spa_crud/server/routes/api.js
+++ projeto/spa_crud/server/routes/api.js
@@ -3,12 +3,12 @@ var express = require('express'),
 
 //routes for user api
 router.use("/user", require("../controllers/user.api"));
+
+//add here other api routes
 router.use("/equipas", require("../routes/equipas"));
 router.use("/jogadores", require("../routes/jogadores"));
 router.use("/jogo", require("../routes/jogo"));
 router.use("/ligas", require("../routes/ligas"));
 router.use("/mensagem", require("../routes/mensagem"));
-
-//add here other api routes
 
 module.exports = router;
