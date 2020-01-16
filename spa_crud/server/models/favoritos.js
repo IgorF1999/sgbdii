f465873e53c444ba778c28b648a8277ddfc698ec
IgorF1999 <43825154+IgorF1999@users.noreply.github.com>
2020-01-16 23:28:07 +0000

    latest update

--- spa_crud/server/models/favoritos.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require("mongoose"),
-    Schema = mongoose.Schema,
-    objectId = mongoose.Schema.ObjectId;
-
-var favoritosSchema = new Schema({ ligas: 'string', equipas: 'string'});
-
-module.exports = favoritosSchema;
--- spa_crud/server/models/pais.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require("mongoose"),
-    Schema = mongoose.Schema,
-    objectId = mongoose.Schema.ObjectId;
-
-var paisSchema = new Schema({ nome: 'string', imagem: 'string'});
-
-module.exports = paisSchema;
